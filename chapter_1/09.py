407dad59e4a60ca677512fb0b3b84bf0c53916e5
Ichinose311 <152970438+Ichinose311@users.noreply.github.com>
2026-04-15 03:46:08 +0900

    リーダブルコード修正

--- chapter_1/09.py
+++ chapter_1/09.py
@@ -1,6 +1,6 @@
 import random
 
-# 意味のある名前を使う（p.18）
+# 明確な単語を選ぶ（p.10）
 text = "I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 
 words = text.split()
@@ -8,7 +8,9 @@ result_text = ""
 
 for word in words:
     if len(word) > 4:
-        first_char = word[0]
+        # コードを「段落」に分割する (p.51)
+        # 説明変数(p.100)
+        first_char = word[0] #範囲を指定するときはfirstとlastを使う（p.32）
         last_char = word[-1]
         middle = word[1:-1]
 
