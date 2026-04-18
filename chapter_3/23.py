ea7b18ab1c50130563e132a15df44c2ac04ab0eb
Ichinose311 <152970438+Ichinose311@users.noreply.github.com>
2026-04-18 13:19:18 +0900

    chapter_3までのコード(出力結果なし)

--- chapter_3/23.py
+++ chapter_3/23.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import re
 
+df = pd.read_json('jawiki-country.json.gz', lines=True) #JSON形式のファイルを読み込む。lines=Trueは、各行が独立したJSONオブジェクトであることを示す。
+uk_text = df.query('title=="イギリス"')["text"].values[0] #title行がイギリスの行を抽出し、text列の値を取り出す。最初の要素を取り出す。
+
+for section in re.findall(r"(=+)([^=]+)\1\n", uk_text):
+    print(f"{section[1].strip()}\t{len(section[0]) - 1}")
--- chapter_3/25.py
+++ chapter_3/25.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import re
 
+df = pd.read_json('jawiki-country.json.gz', lines=True) #JSON形式のファイルを読み込む。lines=Trueは、各行が独立したJSONオブジェクトであることを示す。
+uk_text = df.query('title=="イギリス"')["text"].values[0] #title行がイギリスの行を抽出し、text列の値を取り出す。最初の要素を取り出す。
+uk_texts = uk_text.split("\n")
+# \| : ｜からはじまる行を対象にする
+# (.+?) : 1文字以上の任意の文字を非貪欲マッチで取得する
+# \s=\s* : =の前後に0個以上の空白があることを許容する
+# (.+) : 1文字以上の任意の文字を貪欲マッチで取得する
+# |国名 = イギリス みたいな行からキー:国名、値:イギリスを抽出するための正規表現
+pattern = re.compile("\|(.+?)\s=\s*(.+)")
+result = {}
+for line in uk_texts:
+    r = re.search(pattern, line)
+    if r:
+        result[r[1]] = r[2]
+print(result)
