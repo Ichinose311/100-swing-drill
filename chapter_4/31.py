594513dc2b666762aea38400d8dbb23f8a930410
otukare <cianhesoit@icloud.com>
2026-04-20 00:48:08 +0900

    コードのみ(35まで)

---
+++ chapter_4/31.py
@@ -0,0 +1,17 @@
+import MeCab
+
+# ファイル読み込み
+with open("text.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+tagger = MeCab.Tagger() #形態素解析機を作る
+node = tagger.parseToNode(text) #分解した単語がnodeとして連結されている
+
+while node:
+    # node.featureは次のようになっている
+    # 動詞,自立,*,*,五段・ラ行,基本形,来る,キ,キ
+    # これをカンマで分解してリストにする
+    features = node.feature.split(",")
+    if features[0] == "動詞": #feature[0]->品詞
+        print(node.surface, "->", features[6]) #surfaceはそのままの単語,features[6]は基本形
+    node = node.next
---
+++ chapter_4/34.py
@@ -0,0 +1,34 @@
+import CaboCha
+
+with open("text.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+parser = CaboCha.Parser()
+tree = parser.parse(text)
+
+chunks = []
+current_chunk = None
+
+# 文節作成
+for i in range(tree.size()):
+    token = tree.token(i)
+
+    if token.chunk is not None:
+        current_chunk = {
+            "text": "",
+            "link": token.chunk.link
+        }
+        chunks.append(current_chunk)
+
+    if current_chunk is not None:
+        current_chunk["text"] += token.surface
+
+# 主語「メロス」を含む文節を探す
+for i, chunk in enumerate(chunks):
+    if "メロス" in chunk["text"]:
+        dst = chunk["link"]
+
+        if dst != -1 and dst < len(chunks):
+            print("主語:", chunk["text"])
+            print("述語:", chunks[dst]["text"])
+            print()
